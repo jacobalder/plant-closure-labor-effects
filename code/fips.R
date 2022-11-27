913346f6fdd305f7783d2ec3be360711690a2f05
Jacob Alder <j.cannonalder@gmail.com>
2022-11-26 20:33:14 -0500

    Minor improvements to track Operating plants, too

--- code/fips.R
+++ code/fips.R
@@ -13,24 +13,23 @@ p_load( tigris
        ,stringi)
 
 # Fix fips_codes
-fips = tidycensus::fips_codes
-setDT(fips)
-fips[,county := gsub(" County","",county)]
-fips[,fips:=paste0(state_code,county_code)]
+dt.fips = tidycensus::fips_codes
+setDT(dt.fips)
+dt.fips[,`:=`(county = gsub(" County","",county),
+              fips = paste0(state_code,county_code))]
 
 # Fix County Names
-x <- re[,county:=stringi::stri_trans_totitle(county)]
-setnames(x, old = "plant_state", new = "state")
+fips_gens = gens[,county:=stringi::stri_trans_totitle(county)]
+setnames(fips_gens, old = "plant_state", new = "state")
 
 # Use Lat/Long to look up missing county FIPS -----------------------------
 # if (!require("RJSONIO")) install.packages("RJSONIO")
-# http://www.fcc.gov/developers/census-block-conversions-api
+# https://geo.fcc.gov/api/census/#!/area/get_area
 latlong2fips =
   function(latitude, longitude) {
     if(!is.na(as.numeric(latitude)) & !is.na(as.numeric(longitude))){
       url <- "https://geo.fcc.gov/api/census/area?lat=%s&lon=%s&censusYear=2010&format=json"
       url <- sprintf(url, latitude, longitude)
-      url <- sprintf(url, lat, lon)
       json <- RJSONIO::fromJSON(url)
         if(length(json[["results"]]) == 0){return("0")} else {return(as.character(json[["results"]][[1]][["county_fips"]]))}
     } else {return("0")}
@@ -40,42 +39,39 @@ latlong2fips =
 latlong2fips_convert = 
   function(dt){
     for(i in 1:nrow(dt)){
-      lat = dt[i, latitude]
-      lon = dt[i, longitude]
-      dt[i, fips := latlong2fips(latitude = lat, longitude = lon)]
+      dt[i, fips := latlong2fips(latitude = dt[i, latitude], longitude = dt[i, longitude])]
     }
   }
 
 # # Subset to remove the missing latitudes and longitudes
 lat_lon = c("latitude","longitude")
-a = re[,(lat_lon) := lapply(.SD, as.numeric),.SDcols = lat_lon
-       ][!is.na(latitude),][!is.na(longitude),][,fips:="0"]
-
-# Convert fips for all (doesn't work, function problem, leaving for now)
-latlong2fips_convert(a)
+fips_gens[,(lat_lon) := lapply(.SD, as.numeric),.SDcols = lat_lon]
 
 # Store a list of generators with missing lat/lon
-missing_lat_lon = re[is.na(latitude) | is.na(longitude),]
+missing_lat_lon = fips_gens[is.na(latitude) | is.na(longitude),]
 
+# Drop blank lat/lon
+fips_gens = gens[!is.na(latitude),][!is.na(longitude),]
 
 # Try merging to get county codes -----------------------------------------
 # Merge FIPS codes
-fips_merge = merge(x = x, y = fips, by = c("state","county"), all.x = TRUE)
+fips_merge = merge(x = fips_gens, y = dt.fips, by = c("state","county"), all.x = TRUE)
 fips_merge[is.na(county_code),
            .(n = uniqueN(county)), keyby = state]
 
-# Get the stragglers
-missing_county_codes = fips_merge[is.na(fips),.(ID = unique(ID)), keyby = list(state,county,latitude,longitude,fips)]
+# Get the stragglers (FYI, I checked that the plant ids are unique on a state,county,lat_lon basis)
+missing_county_codes = fips_merge[is.na(fips),.(plant_id = unique(plant_id)),
+                                    keyby = list(state,county,latitude,longitude,fips,entity_id)]
 
-# Convert fips on missing county codes
+# Convert fips on missing county codes (this takes a while)
 latlong2fips_convert(missing_county_codes)
 
 # Add back in the missing county codes
 fips_merge_fix = merge(x = fips_merge, y = missing_county_codes, 
-                       by = "ID", all.x = TRUE)
+                       by = c("entity_id","plant_id"), all.x = TRUE)
 
 # Clean up ---------------------------------------------------------------
-re_cleaned = fips_merge_fix[,`:=`(fips=fifelse(test = 
+gens_cleaned = fips_merge_fix[,`:=`(fips=fifelse(test = 
                                                  is.na(fips.x),
                                                yes = fips.y,
                                                no = fips.x),
@@ -98,23 +94,17 @@ re_cleaned = fips_merge_fix[,`:=`(fips=fifelse(test =
                             ]
 
 # Drop the merged cols
-re_c <- re_cleaned %>% 
+gens_c <- gens_cleaned %>% 
   dplyr::select(-contains(c('.x', '.y')))
 
+# Save vars
+save(dt.fips,gens_c,missing_county_codes,missing_lat_lon,file = file.path(data.dir,"Rdata","fips.Rdata"))
+
 # Clean Environment
-rm(a
-   ,fips_merge
+rm( fips_merge
    ,fips_merge_fix
-   ,missing_county_codes
-   ,re_cleaned
-   ,x
-   ,i
-   ,lat
-   ,lon
-   ,lat_lon
-   ,url
-   ,merged_cols_to_drop
+   ,fips_gens
+   ,gens_cleaned
    ,latlong2fips
    ,latlong2fips_convert)
-
 # -------------------------------------------------------------------------
